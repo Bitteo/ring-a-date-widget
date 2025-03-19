0feb0bff3128bf1d98257759a17f01dec048af4b
Jigoweb <matteo.foroni@jigoweb.com>
2025-03-19 01:26:01 +0100

    created new xcode project

---
+++ xcode-ring-a-date/xcode-ring-a-date/xcode_ring_a_dateApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_ring_a_dateApp.swift
+//  xcode-ring-a-date
+//
+//  Created by Matteo Foroni on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct xcode_ring_a_dateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcode-ring-a-date/xcode-ring-a-dateTests/xcode_ring_a_dateTests.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_ring_a_dateTests.swift
+//  xcode-ring-a-dateTests
+//
+//  Created by Matteo Foroni on 19/03/25.
+//
+
+import Testing
+@testable import xcode_ring_a_date
+
+struct xcode_ring_a_dateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
